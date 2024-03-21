7e5a6b17f271e710799499de144d98ee9b0f688c
Hayden Linder <haydenlinder@gmail.com>
2024-03-21 10:39:57 -0700

    Loayout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`mt-16 m-10 sm:ml-[240px] p-10 flex justify-center`}>
+        <body className={`m-16 sm:ml-[300px] p-10 flex justify-center`}>
           <div className="container">
             <ApolloWrapper>
               <AppRouterCacheProvider options={{}}>
