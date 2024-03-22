3817ae592a3fa8f79297aef69dfc6d03ded910c4
Hayden Linder <haydenlinder@gmail.com>
2024-03-21 23:30:04 -0700

    Route

--- frontend/middleware.ts
+++ frontend/middleware.ts
@@ -2,7 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
   // Routes that can be accessed while signed out
-  publicRoutes: ["/", "/api/webhooks(.*)", "/app/api/webhooks(.*)"],
+  publicRoutes: ["/", "/app", "/api/webhooks(.*)", "/app/api/webhooks(.*)"],
   // Routes that can always be accessed, and have
   // no authentication information
   ignoredRoutes: ["/about"],
