941b268f501622c2b070125dc624814fc155093f
Hayden Linder <haydenlinder@gmail.com>
2024-03-21 23:25:53 -0700

    homepage

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,9 +3,10 @@
 import { PostForm } from "@/components/PostForm";
 import { Posts } from "@/components/Posts";
 import { ModalTypes, useModalStore } from "@/state/store";
-import { SignIn, SignedIn, useAuth } from "@clerk/nextjs";
+import { SignIn, SignInButton, SignedIn, useAuth } from "@clerk/nextjs";
 import AddIcon from "@mui/icons-material/Add";
-import { Button, Modal, Paper } from "@mui/material";
+import { Button, Modal, Paper, Typography } from "@mui/material";
+import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
@@ -14,5 +15,16 @@ export default function Home() {
   const { update } = useModalStore();
   const { userId } = useAuth();
 
-  return <div className="w-full flex flex-col items-center">Do stuff</div>;
+  return (
+    <div className="w-full flex flex-col items-center">
+      <Paper className="w-full flex flex-col items-center mb-20 mt-10">
+        <Typography variant="h1">Do stuff</Typography>
+        <Typography variant="h1">But cooler</Typography>
+      </Paper>
+
+      <Link href={"/app"}>
+        <Button variant="contained">Get Started</Button>
+      </Link>
+    </div>
+  );
 }
